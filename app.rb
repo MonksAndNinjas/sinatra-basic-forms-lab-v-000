a41c69d81b4e1e8c8583e1cabfefebb7effaff6a
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-07-02 02:56:43 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
-  post '/NEW' do
+  post '/new' do
     @puppy = Puppy.new(params[:name], params[:breed], params[:age])
 
     erb :display_puppy
